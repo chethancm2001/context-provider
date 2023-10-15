b1992311ef887715fbc78e3f70f6e5d54de94620
chethancm2001 <chethancm2001kadur@gmail.com>
2023-10-15 21:43:03 +0530

    updated the file

--- entry.py
+++ entry.py
@@ -15,15 +15,18 @@ load_dotenv()
 class ContextProvider:
     #global variables
     openai_url = "https://chat.openai.com/auth/login"
+    
 
-    def __init__(self,Email,password):
+    def __init__(self,Email,password,path="./"):
         # instance variables
         self.email = Email
         self.password = password
+        self.path = path
         # settings up the driver
         options = uc.ChromeOptions() 
-        options.headless = False 
-        self.driver = uc.Chrome(use_subprocess=True, options=options) 
+        options.headless = False
+        self.driver = uc.Chrome(use_subprocess=True, options=options)
+        self.wait = WebDriverWait(self.driver, 10) 
     
     def login(self):
         self.driver.get(self.openai_url) 
@@ -43,69 +46,97 @@ class ContextProvider:
         time.sleep(1)
         final_submit = self.driver.find_elements(By.XPATH,"//button[text()='Continue']")
         final_submit[1].click()
-        time.sleep(10)
+        
  
+    def create_new_chat(self):
+        escape = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="radix-:rh:"]/div[2]/div/div[4]/button')))
+        escape.click()
+        new_chat = self.driver.find_elements(By.XPATH,"//a")
+        new_chat[0].click()
+        
+    def gettree(self):
+        path = self.path
+        def is_not_hidden(path):
+            return not path.name.startswith(".")
+# 
+        paths = DisplayablePath.make_tree(Path(self.path), criteria=is_not_hidden)
+        string = ""
+        for path in paths:
+            string += path.displayable()
+            string += "\n"  
+        return string
+    
+    def context_tree(self):
+        initial = r"You are a code interpreter and summariser. You'll receive file tree of the project and also the contents of individual files. You'll analyze the code and write a brief summary about the project and also respond to queries within the context. The file tree will be sent now but the code of each files will be sent in subsequent messages. Do not reply or make any commentary until i instruct you so in curly braces as {START}. The file tree:"
+        tree = self.gettree()
+        textarea = self.driver.find_element(By.XPATH,"//textarea")
+        textarea.send_keys(initial)
+        time.sleep(2)
+        send = self.driver.find_element(By.XPATH,"//div/button[@data-testid='send-button']")
+        send.click()
+        time.sleep(2)
+        textarea.send_keys(tree)
+        time.sleep(2)
+        wait = WebDriverWait(self.driver, 10)
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, "//div/button[@data-testid='send-button']")))
+        button.click()
+        time.sleep(5)
 
+    def senddata(self,data):
+        textarea = self.driver.find_element(By.XPATH,"//textarea")
+        textarea.send_keys(data)
+        time.sleep(4)
+        wait = WebDriverWait(self.driver, 10)
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, "//div/button[@data-testid='send-button']")))
+        button.click()
+        time.sleep(4)
+
+    
+    def sendContent(self):
+        print("sending content")
+        # directory_path = self.path
+        for root, dirs, files in os.walk("./"):
+          
+            dirs[:] = [d for d in dirs if not d.startswith('.')]
+            
+            for file in files:
+                file_path = os.path.join(root, file)
+
+                # Check if the path is a file (not a directory)
+                if os.path.isfile(file_path):
+                    try :
+                        with open(file_path, 'r') as f:
+            
+                            file_contents = f.read()
+                            print(f"filename:{file}" + "\n"+"\n" +file_contents)
+                            print("\n")
+                            textarea = self.driver.find_element(By.XPATH,"//textarea")
+                            textarea.send_keys(f"filename:{file} {file_contents}")
+                            wait = WebDriverWait(self.driver, 10)
+                            button = wait.until(EC.element_to_be_clickable((By.XPATH, "//div/button[@data-testid='send-button']")))
+                            button.click()
+                            time.sleep(4)
+                        
+                    except:
+                        print(f"error in {file}")
+                        
+
+            # You can read the contents of the file here
+          
+          
+          
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 
 
 con = ContextProvider(email,password)
 con.login()
+con.create_new_chat()
+con.context_tree()
+con.sendContent()
 
 
 
-def createNewChat():
-    escape = driver.find_elements(By.XPATH,"//button[@as='button']")
-    escape[-1].click()
-    new_chat = driver.find_elements(By.XPATH,"//a")
-    new_chat[0].click()
-    time.sleep(2)
-
-def send_tree():
-    initial = r"You are a code interpreter and summariser. You'll receive file tree of the project and also the contents of individual files. You'll analyze the code and write a brief summary about the project and also respond to queries within the context. The file tree will be sent now but the code of each files will be sent in subsequent messages. Do not reply or make any commentary until i instruct you so in curly braces as {START}. The file tree:"
-    tree = gettree()
-
-    textarea = driver.find_element(By.XPATH,"//textarea")
-    textarea.send_keys(initial)
-    time.sleep(2)
-
-    send = driver.find_element(By.XPATH,"//div/button[@data-testid='send-button']")
-    send.click()
-    time.sleep(2)
-
-
-    textarea.send_keys(tree)
-    time.sleep(2)
-    wait = WebDriverWait(driver, 10)
-    button = wait.until(EC.element_to_be_clickable((By.XPATH, "//div/button[@data-testid='send-button']")))
-    button.click()
-    time.sleep(2)
-
-def sendContent():
-    directory_path = "./"
-
-    # Loop through all files in the directory and its subdirectories
-    for root, dirs, files in os.walk("./"):
-        # Filter out hidden folders (those starting with a dot)
-        dirs[:] = [d for d in dirs if not d.startswith('.')]
-        
-        for file in files:
-            file_path = os.path.join(root, file)
-
-            # Check if the path is a file (not a directory)
-            if os.path.isfile(file_path):
-                try :
-                      with open(file_path, 'r') as f:
-         
-                        file_contents = f.read()
-                        data = f"filename:{file}"+ "\n" + file_contents
-                        senddata(data)
-                    
-                except:
-                    pass
-                # You can read the contents of the file here
-              
 
 
   
@@ -117,28 +148,13 @@ def sendContent():
 
     
 
-def gettree():
-    def is_not_hidden(path):
-        return not path.name.startswith(".")
-# 
-    paths = DisplayablePath.make_tree(Path('./'), criteria=is_not_hidden)
-    string = ""
-    for path in paths:
-        string += path.displayable()
-        string += "\n"  
-    return string
 
 
 
 
 
-def senddata(data):
-    textarea = driver.find_element(By.XPATH,"//textarea")
-    textarea.send_keys(data)
-    time.sleep(2)
-    button = WebDriverWait.until(EC.element_to_be_clickable((By.XPATH, "//div/button[@data-testid='send-button']")))
-    button.click()
-    time.sleep(2)
+
+
 
 
 
